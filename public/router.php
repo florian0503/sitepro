40da85ec2bd3cbbcc31e5856c4c88082ae01a322
emilienelumba <emilienelumba@gmail.com>
2026-01-26 14:19:53 +0100

    clean: suppression fichiers non nécessaires et mise à jour gitignore

--- public/router.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (is_file($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $_SERVER['SCRIPT_NAME'])) {
-    return false;
-}
-
-$_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'index.php';
-
-require 'index.php';
