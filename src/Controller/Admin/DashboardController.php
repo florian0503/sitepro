7834a8987fcef1b4eb33dffe5df0eebd0ac3b546
florian0503 <florian.dimbert2@mail.com>
2026-04-13 00:17:42 +0200

    feat: mode maintenance avec page chantier et toggle admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -14,18 +14,55 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    private string $lockFile;
+
+    public function __construct(
+        #[Autowire('%kernel.project_dir%')]
+        string $projectDir,
+    ) {
+        $this->lockFile = $projectDir.'/var/maintenance.lock';
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
         return $this->redirect($adminUrlGenerator->setController(RealisationCrudController::class)->generateUrl());
+    }
+
+    #[Route('/admin/maintenance', name: 'admin_maintenance', methods: ['GET'])]
+    public function maintenance(): Response
+    {
+        return $this->render('admin/maintenance.html.twig', [
+            'maintenance_active' => file_exists($this->lockFile),
+        ]);
+    }
+
+    #[Route('/admin/maintenance/toggle', name: 'admin_maintenance_toggle', methods: ['POST'])]
+    public function maintenanceToggle(Request $request): Response
+    {
+        if (!$this->isCsrfTokenValid('maintenance_toggle', (string) $request->request->get('_token'))) {
+            throw $this->createAccessDeniedException('Token CSRF invalide.');
+        }
+
+        if (file_exists($this->lockFile)) {
+            unlink($this->lockFile);
+            $this->addFlash('success', 'Mode maintenance désactivé.');
+        } else {
+            touch($this->lockFile);
+            $this->addFlash('warning', 'Mode maintenance activé.');
+        }
+
+        return $this->redirectToRoute('admin_maintenance');
     }
 
     public function configureDashboard(): Dashboard
@@ -48,6 +85,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Réalisations', 'fa fa-images', Realisation::class);
         yield MenuItem::linkToCrud('Catégories', 'fa fa-folder', Category::class);
         yield MenuItem::section('');
+        yield MenuItem::linkToRoute('Mode maintenance', 'fa fa-tools', 'admin_maintenance');
         yield MenuItem::linkToRoute('Voir le site', 'fa fa-eye', 'app_home');
         yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out');
     }
